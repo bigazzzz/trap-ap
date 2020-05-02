dc29e61a35b7d48521c250f0a21cbc2285a7f12f
bigazzzz <bigazzzz@gmail.com>
2020-05-02 15:26:40 +0300

    trap.sh tested

--- trap.sh
+++ trap.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-WIFI_IFACE=wpl2s0
+WIFI_IFACE=wlp2s0
 ETH_IFACE=enp3s0
-sudo apt update
-sudo apt install hostapd dnsmasq
-sudo cp dnsmasq.conf.small /etc/dnsmasq.conf
+[ ! -f /etc/dnsmasq.conf ] && sudo apt install dnsmasq -y
+[ ! -f /etc/hostapd/hostapd.conf ] && sudo apt install hostapd -y
+sudo cp dnsmasq.conf /etc/dnsmasq.conf
+sudo sed -i "s/wlan0/$WIFI_IFACE/g" /etc/dnsmasq.conf
+sudo sed -i "s/eth0/$ETH_IFACE/g" /etc/dnsmasq.conf
 sudo cp hostapd.conf /etc/hostapd/hostapd.conf
 sudo echo 1 > /proc/sys/net/ipv4/ip_forward
+sudo sed -i "s/wlan0/$WIFI_IFACE/g" /etc/hostapd/hostapd.conf
+sudo sed -i "s/eth0/$ETH_IFACE/g" /etc/hostapd/hostapd.conf
 sudo iptables -tnat -A POSTROUTING -o $ETH_IFACE -j MASQUERADE
 sudo echo nameserver 8.8.8.8 >> /etc/resolv.conf
 sudo echo nameserver 8.8.4.4 >> /etc/resolv.conf
-sudo ip addr add 192.168.2.1/24 dev $WIFI_IFACE
+sudo iw dev $WIFI_IFACE disconnect
+sudo ip addr replace 192.168.2.1/24 dev $WIFI_IFACE
 sudo service hostapd start
 sudo service dnsmasq start
 
